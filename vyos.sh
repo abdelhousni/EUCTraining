94bdd9578266b35a02390ef96a83253f3b1deceb
housni.h <23284113+AbdellatifHousni@users.noreply.github.com>
2019-10-20 00:30:19 +0200

    Update vyos.sh

--- vyos.sh
+++ vyos.sh
@@ -1,16 +1,23 @@
-# source : http://z0b.kapsi.fi/vyatta_vbox.php
-# Message "INIT: Id "TO" respawning too fast: disabled for 5 minutes"
-delete system console
-# change keyboard layout
-sudo dpkg-reconfigure keyboard-configuration
+# different problems
+## source : http://z0b.kapsi.fi/vyatta_vbox.php
+## Message "INIT: Id "TO" respawning too fast: disabled for 5 minutes"
+$ delete system console
+## change keyboard layout
+$ sudo dpkg-reconfigure keyboard-configuration
 
 
-# activate ssh
+## activate ssh
 set service ssh
-# activate snmp
-set service snmp community cpas
-set service snmp contact sysadmin@cpasbxl.brussels
+## activate snmp
+configure
+set service snmp community public
+set service snmp contact admin@localdomain
 set service snmp listen-address 172.22.23.xxx
+save
 
-# source : https://vyos.readthedocs.io/en/latest/services/ssh.html
-# add ssh public keys
+## add ssh public keys
+## source : https://vyos.readthedocs.io/en/latest/services/ssh.html
+configure
+set system login user vyos authentication public-keys 'identifier' key "AAAAB3Nz...."
+set system login user vyos authentication public-keys 'identifier' type ssh-rsa"
+commit
